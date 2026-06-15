f397021e217eead7745b0ac69060d8f62178585d
Domenico Violi <dome.violi90@gmail.com>
2026-06-15 11:24:50 +0200

    Update ProfileSummary.tsx

--- src/components/ProfileSummary.tsx
+++ src/components/ProfileSummary.tsx
@@ -2,11 +2,9 @@ import React from "react";
 import type { CVData } from "../types/interfaces";
 
 export const ProfileSummary: React.FC<{ data: CVData }> = ({ data }) => (
-  <section className='mb-10 print-keep-together'>
-    <div className='border-l-4 border-blue-200 pl-5 py-1'>
-      <p className='text-slate-700 text-[15.5px] leading-relaxed max-w-[95%] whitespace-pre-wrap'>
-        {data.summary}
-      </p>
-    </div>
+  <section className='mb-4 print-keep-together'>
+    <p className='text-gray-750 text-[13px] leading-relaxed text-justify italic'>
+      {data.summary}
+    </p>
   </section>
 );
