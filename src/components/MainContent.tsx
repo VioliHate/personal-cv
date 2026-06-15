48ffe883d4df9f6458c4964c19e4275bf3f21b86
Domenico Violi <dome.violi90@gmail.com>
2026-06-15 11:43:03 +0200

    Update MainContent.tsx

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -4,24 +4,24 @@ import { ExperienceSection } from "./ExperienceSection";
 import { ProfileSummary } from "./ProfileSummary";
 import { TechStack } from "./TechStack";
 import { Education } from "./Education";
+import { Languages } from "./Languages";
 
 export const MainContent: React.FC<{ data: CVData }> = ({ data }) => {
   return (
     <main>
       {/* Profilo professionale */}
       <ProfileSummary data={data} />
-
       {/* Tech Stack - Tag minimali */}
       <TechStack data={data} />
-
       <ExperienceSection title='Work Experience' items={data.workExperience} />
       <ExperienceSection
         title='Side Projects'
         items={[...data.outWorkExperience].reverse()}
       />
-
       {/* Formazione */}
       <Education data={data} />
+      {/* Lingue */}
+      <Languages data={data} />{" "}
     </main>
   );
 };
