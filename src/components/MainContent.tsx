516752ea4b003d6b17464e8c48b7743bc3c6ae82
Domenico Violi <dome.violi90@gmail.com>
2026-06-15 15:55:56 +0200

    Update MainContent.tsx
    commentato per ridurre gli spazi, non lo cancello nel caso possa riservirmi in futuro

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import type { CVData } from "../types/interfaces";
 import { ExperienceSection } from "./ExperienceSection";
-import { ProfileSummary } from "./ProfileSummary";
 import { TechStack } from "./TechStack";
 import { Education } from "./Education";
 import { Languages } from "./Languages";
@@ -10,7 +9,7 @@ export const MainContent: React.FC<{ data: CVData }> = ({ data }) => {
   return (
     <main>
       {/* Profilo professionale */}
-      <ProfileSummary data={data} />
+      {/* <ProfileSummary data={data} /> */}
       {/* Tech Stack - Tag minimali */}
       <TechStack data={data} />
       <ExperienceSection title='Work Experience' items={data.workExperience} />
